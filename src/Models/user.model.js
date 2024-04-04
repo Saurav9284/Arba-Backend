4809f973e7e0bc797b212af9fcf54d279f688fed
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-04-04 10:02:25 +0530

    Category schema

--- src/Models/user.model.js
+++ src/Models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     userName:{type:String,required:true},
     email:{type:String,required:true},
     password:{type:String,required:true},
-    // avatar:{type:String,required:true}
+    avatar:{type:String}
 })
 
 const UserModel = mongoose.model('User',userSchema)
